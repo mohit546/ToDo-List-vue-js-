a7665b51be91b7b05b7d1eeea5e3784d5d90cba3
Mohit Patil <mohitpatil546@gmail.com>
2016-01-30 12:39:20 +0530

    login server url is added

--- ToDo-List/public/static/js/login.js
+++ ToDo-List/public/static/js/login.js
@@ -2,13 +2,22 @@ new Vue({
 	el: '#login',
 
 	data: {
-		userName: '',
+		userEmail: '',
 		password: ''
 	},
 
 	methods: {
 		login: function(){
-			alert(userName, password);
+			this.$http.post('/api/login/', {userEmail: this.userEmail, password: this.password}).then(function (response) {
+				if(!response.data.status){
+					console.log(response.data);
+				}else{
+					localStorage.setItem('token', response.data.token);
+					window.location.href = response.data.redirectUrl;
+				}
+			}, function (response) {
+				console.log(response.data);
+			});
 		}
 	}
 });
